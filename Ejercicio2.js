94d89fa992f7cef5e14b33ffbb7338b437785830
Sarai Martínez <saraim.puig@gmail.com>
2025-02-16 20:34:56 +0100

    ejercicio2 solucion error

--- Ejercicio2.js
+++ Ejercicio2.js
@@ -3,27 +3,32 @@ for (var i=2; i < numero; i++){
     if (numero % i == 0){
         return false;
     }
+    
 }
+return true;
 
 }
 
 function esPalindromo(cadena) {
-    cadena = numero.toString();
-    cadenaReverse = cadena.reverse();
+    
+    let cadenaReverse = cadena.split("").reverse().join("");
     if (cadena == cadenaReverse){
     return true;
     }
+    return false;
 }
 
 var numeroPrompt = parseInt(prompt("Introduce el número X (se comprobará desde 1 hasta X)"));
 
-var arrayPrimos, arrayPalindromo, arrayAmbos = [];
+var arrayPrimos = [];
+var arrayPalindromo = [];
+var arrayAmbos = [];
 for (var i=1; i<=numeroPrompt; i++){
  var primo = esPrimo(i);
  if (primo){
     arrayPrimos.push(i);
  }
- var palindromo = esPalindromo(i);
+ var palindromo = esPalindromo(i.toString());
  if (palindromo){
     arrayPalindromo.push(i);
  }
